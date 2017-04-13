e448590fdbd47708680f21a00fc63c854607db7a
Adam Cribbs <adamc@Adams-MBP-6.home>
2017-04-13 22:59:34 +0100

    added compression

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_googlemaps import GoogleMaps
+from flask_compress import Compress
 
 app = Flask(__name__)
+Compress(app)
 from app import views
 
 GoogleMaps(app, key="AIzaSyBKZ8HTbkOmKT2oaJ_8p8djs02C4OlF44I")
