1061c565b87b41685a2c2720469bedbccd8db32f
Acribbs <adam.cribbs@imm.ox.ac.uk>
2020-04-15 12:48:27 +0100

    replaced flask website with Rmarkdown

--- app/Models.py
+++
@@ -1,4 +0,0 @@
-from flask import Flask, redirect, url_for, render_template
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from app import app
--- run.py
+++
@@ -1,7 +0,0 @@
-#!flask/bin/python
-from app import app
-
-#Config
-app.config.from_object('config.DevelopmentConfig')
-
-app.run()
